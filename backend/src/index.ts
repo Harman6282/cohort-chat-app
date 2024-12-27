a9441f6ebd0352f4b4e568786289f54fdd09198b
Harman6282 <harmans6282@gmail.com>
2024-12-27 22:07:50 +0530

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,34 @@
-import { WebSocketServer , WebSocket } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 interface User {
   socket: WebSocket;
-  room: string
+  room: string;
 }
 
 let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-
   socket.on("message", (message) => {
     //@ts-ignore
     let parsedMessage = JSON.parse(message);
 
-    if(parsedMessage.type == "join"){
+    if (parsedMessage.type == "join") {
       allSockets.push({
         socket,
-        room: parsedMessage.payload.roomId
-      })
-      
+        room: parsedMessage.payload.roomId,
+      });
     }
 
-    if(parsedMessage.type == "chat"){
+    if (parsedMessage.type == "chat") {
       const currentUserRoom = allSockets.find((x) => x.socket == socket)?.room;
 
-      for(let i = 0; i < allSockets.length; i++){
-        if(allSockets[i].room == currentUserRoom){
-          allSockets[i].socket.send(parsedMessage.payload.message)
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].room === currentUserRoom) {
+          allSockets[i].socket.send(parsedMessage.payload.message);
         }
       }
-
     }
-
-   
   });
-
-
-  
-
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,80 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
-import './App.css'
 
 function App() {
+  const [messages, setMessages] = useState(["hello"]);
+  const inputRef = useRef();
+  const wsRef = useRef();
+
+  useEffect(() => {
+    const ws = new WebSocket("http://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages((m) => [...m, event.data]);
+    };
+  
+    wsRef.current = ws;
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: "red",
+          },
+        })
+      );
+    };
+
+    return () => {
+      ws.close()
+    }
+  }, []);
+
+  console.log(messages)
 
   return (
     <>
-      <div className='border-2 border-white p-5 w-96'>
-        <h1 className='text-xl'>Chat app</h1>
-        <div>
+      <div className="flex flex-col h-[95vh] bg-gray-100">
+        {/* Chat Messages */}
+        <div className="flex-1 p-4 overflow-y-auto bg-gray-50 space-y-4">
+          <div className="flex">
+            <div className=" rounded-lg p-3 max-w-xs flex flex-col items-start gap-2">
+              {messages.map((message) => (
+                <div className="bg-gray-200 p-2 rounded-md ">{message}</div>
+              ))}
+            </div>
+          </div>
+         
+        </div>
 
+        {/* Chat Input */}
+        <div className="p-4 bg-white border-t flex items-center space-x-2">
+          <input
+            ref={inputRef}
+            type="text"
+            className="flex-1 p-3 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+            placeholder="Type a message..."
+          />
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+            onClick={() => {
+              const message = inputRef.current?.value;
+              wsRef.current.send(
+                JSON.stringify({
+                  type: "chat",
+                  payload: {
+                    message: message,
+                  },
+                })
+              );
+            }}
+          >
+            Send
+          </button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
